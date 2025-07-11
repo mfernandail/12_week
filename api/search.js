ef1ac21cbb9bada99cd699033f3753065e3f43d5
mfernandail <maria.ibanez.leiva@gmail.com>
2025-07-10 21:20:21 -0400

    Refactor search API to use environment variable for API key and improve error handling

--- api/search.js
+++ api/search.js
@@ -1,1 +1,23 @@
-const API_KEY = 'bfca5c2d';
+export default async function (req, res) {
+  const { query, page } = req.query
+  const API_KEY = process.env.OMDB_API_KEY
+
+  if (!query) {
+    return res.status(400).json({ error: 'No query provided' })
+  }
+
+  try {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}&page=${page}`
+    )
+    const data = await response.json()
+
+    if (data.Response === 'False') {
+      return res.status(404).json({ error: data.Error })
+    }
+
+    return res.status(200).json(data)
+  } catch (error) {
+    return res.status(400).json({ error: 'Server error' })
+  }
+}
