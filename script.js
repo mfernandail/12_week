ce5d338036d8879699f093c71c357e927646a148
mfernandail <maria.ibanez.leiva@gmail.com>
2025-07-13 00:36:24 -0400

    Enhance favorites functionality with improved view toggling and local storage management

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const btnForm = document.querySelector('.form_btn')
 const inputForm = document.querySelector('.form_inputSearch')
 const result = document.querySelector('#results')
+const favorites = document.querySelector('#favorites')
 const titleForm = document.querySelector('main h1')
 const changeView = document.querySelector('#changeView')
+
+const formSearch = document.querySelector('#form_search')
 
 const pagination = document.querySelector('.pagination')
 const prevBtn = document.getElementById('prevBtn')
 const nextBtn = document.getElementById('nextBtn')
 const pageInfo = document.getElementById('pageInfo')
 
-let favoritesLocalStorage =
-  JSON.parse(localStorage.getItem('moviesFavorites')) || []
-
 let currentPage = 1
 let totalPages = 1
 let currentQuery = ''
+
+let favoritesLocalStorage =
+  JSON.parse(localStorage.getItem('moviesFavorites')) || []
 
 titleForm.addEventListener('click', () => {
   result.innerHTML = ''
@@ -30,25 +33,96 @@ inputForm.addEventListener('keydown', (e) => {
 
 changeView.addEventListener('click', changeViewFn)
 
+result.addEventListener('click', addFn)
+favorites.addEventListener('click', remFn)
+
 function changeViewFn(e) {
   if (e.target.value === 'Favorites') {
+    favorites.classList.remove('hide_resultFav')
+    result.classList.add('hide_resultFav')
+    formSearch.classList.add('hide_resultFav')
+    pagination.classList.add('hide_resultFav')
+
     e.target.value = 'Search movies'
+
+    createCardFavorites()
+    // favoritesLocalStorage.forEach((fav) => {
+    //   const card = document.createElement('article')
+    //   card.classList.add('result_card')
+
+    //   card.innerHTML = `
+    //     <h1 class="card_title">${fav.title}</h1>
+    //     <img src=${fav.poster} alt=${fav.title} class="card_image" onerror="this.onerror=null; this.src='./images/no_found.png';" />
+    //     <a href="#" data-id=${fav.imdbID} class="addRem">Remove</a>
+    //   `
+    //   favorites.appendChild(card)
+    // })
   } else {
     e.target.value = 'Favorites'
+    favorites.classList.add('hide_resultFav')
+    result.classList.remove('hide_resultFav')
+    formSearch.classList.remove('hide_resultFav')
+    pagination.classList.remove('hide_resultFav')
   }
 }
 
-result.addEventListener('click', addRemFn)
+function createCardFavorites() {
+  favorites.innerHTML = ''
+  favoritesLocalStorage.forEach((fav) => {
+    const card = document.createElement('article')
+    card.classList.add('result_card')
 
-function addRemFn(e) {
+    card.innerHTML = `
+        <h1 class="card_title">${fav.title}</h1>
+        <img src=${fav.poster} alt=${fav.title} class="card_image" onerror="this.onerror=null; this.src='./images/no_found.png';" />
+        <a href="#" data-id=${fav.imdbID} class="addRem">Remove</a>
+      `
+    favorites.appendChild(card)
+  })
+}
+
+function remFn(e) {
   e.preventDefault()
+  const movie = e.target.parentElement
+
+  if (e.target.classList.contains('addRem')) {
+    const imdbID = movie.querySelector('a').getAttribute('data-id')
+    console.log(imdbID)
+
+    favoritesLocalStorage = favoritesLocalStorage.filter(
+      (movie) => movie.imdbID !== imdbID
+    )
+
+    localStorage.setItem(
+      'moviesFavorites',
+      JSON.stringify(favoritesLocalStorage)
+    )
+
+    createCardFavorites()
+  }
+}
+
+function addFn(e) {
+  e.preventDefault()
+
+  const movie = e.target.parentElement
+
+  const existFav = favoritesLocalStorage.some(
+    (fav) => fav.imdbID === movie.querySelector('a').getAttribute('data-id')
+  )
+
+  if (existFav) return
+
   if (e.target.classList.contains('addFav')) {
-    const movie = e.target.parentElement
+    const poster =
+      movie.querySelector('img').src && movie.querySelector('img').src !== 'N/A'
+        ? movie.querySelector('img').src
+        : './imeges/no_found.png'
 
     const infoMovie = {
       title: movie.querySelector('h1').textContent,
-      poster: movie.querySelector('img').src,
-      imdbID: movie.querySelector('a').getAttribute('id-data'),
+      poster: poster, //movie.querySelector('img').src,
+      imdbID: movie.querySelector('a').getAttribute('data-id'),
     }
     favoritesLocalStorage = [...favoritesLocalStorage, infoMovie]
 
@@ -56,8 +130,6 @@ function addRemFn(e) {
       'moviesFavorites',
       JSON.stringify(favoritesLocalStorage)
     )
-  } else if (e.target.classList.contains('addRem')) {
-    console.log('rem')
   }
 }
 
@@ -115,12 +187,11 @@ async function fetchMovie(title, page = 1) {
 
 function createCardMovie(data) {
   result.innerHTML = ''
-  console.log(data)
   data.forEach((movie) => {
     const poster =
       movie.Poster && movie.Poster !== 'N/A'
         ? movie.Poster
-        : '.imeges/no_found.png'
+        : './imeges/no_found.png'
 
     const card = document.createElement('article')
     card.classList.add('result_card')
@@ -128,8 +199,7 @@ function createCardMovie(data) {
     card.innerHTML = `
       <h1 class="card_title">${movie.Title}</h1>
       <img src=${poster} alt=${movie.Title} class="card_image" onerror="this.onerror=null; this.src='./images/no_found.png';" />
-      <a href="#" id-data=${movie.imdbID} class="addFav">Add</a>
-      <a href="#" id-data=${movie.imdbID} class="addRem">Remove</a>
+      <a href="#" data-id=${movie.imdbID} class="addFav">❤️</a>
       `
     result.appendChild(card)
   })
